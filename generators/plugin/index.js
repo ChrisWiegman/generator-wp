f201592fba07001734411bedac5a2fbe1aca4739
Chris Wiegman <chris@chriswiegman.com>
2021-12-23 18:40:43 -0500

    Ensure all testing and other CI stuff works

--- generators/plugin/index.js
+++ generators/plugin/index.js
@@ -1,5 +1,5 @@
-const Generator = require('yeoman-generator');
-const TitleCase = require('title-case');
+const Generator = require("yeoman-generator");
+const TitleCase = require("title-case");
 module.exports = class extends Generator {
   /**
    * Edit Generator options
@@ -11,7 +11,7 @@ module.exports = class extends Generator {
    */
   constructor(args, opts) {
     // Disables npm install by default.
-    super(args, opts, {customInstallTask: (preferredPm) => { }});
+    super(args, opts, { customInstallTask: (preferredPm) => {} });
   }
 
   /**
@@ -25,23 +25,21 @@ module.exports = class extends Generator {
     // eslint-disable-next-line max-len
     const longOrgPrefixLowercase = this.answers.longOrgPrefix.toLowerCase();
     const pluginUnderscore = this.appname
-        .trim()
-        .replace(/[ .-]/g, '_')
-        .toLowerCase();
-    const pluginDash = pluginUnderscore.replace(/_/g, '-');
-    const pluginNoSpaces = pluginDash.replace(/-/g, '');
+      .trim()
+      .replace(/[ .-]/g, "_")
+      .toLowerCase();
+    const pluginDash = pluginUnderscore.replace(/_/g, "-");
+    const pluginNoSpaces = pluginDash.replace(/-/g, "");
     const pluginConst =
-    this.answers.longOrgPrefix.toUpperCase() +
-      '_' +
+      this.answers.longOrgPrefix.toUpperCase() +
+      "_" +
       pluginUnderscore.toUpperCase();
-    const packageName = this.answers.pluginName
-        .trim()
-        .replace(/[ .-]/g, '_');
+    const packageName = this.answers.pluginName.trim().replace(/[ .-]/g, "_");
 
     return {
       pluginName:
-      this.answers.pluginName.charAt(0).toUpperCase() +
-      this.answers.pluginName.slice(1),
+        this.answers.pluginName.charAt(0).toUpperCase() +
+        this.answers.pluginName.slice(1),
       longOrgPrefix: this.answers.longOrgPrefix,
       authorName: this.answers.authorName,
       authorEmail: this.answers.authorEmail,
@@ -67,59 +65,59 @@ module.exports = class extends Generator {
   async prompting() {
     this.answers = await this.prompt([
       {
-        type: 'input',
-        name: 'pluginName',
-        message: 'The Plugin Name',
+        type: "input",
+        name: "pluginName",
+        message: "The Plugin Name",
         // Default to current folder name
         default: TitleCase.titleCase(this.appname),
       },
       {
-        type: 'input',
-        name: 'longOrgPrefix',
-        message: 'The organization name to use as a prefix. Alpha only',
-        default: 'ACME_Plugin_Company',
+        type: "input",
+        name: "longOrgPrefix",
+        message: "The organization name to use as a prefix. Alpha only",
+        default: "ACME_Plugin_Company",
       },
       {
-        type: 'input',
-        name: 'orgPrefix',
+        type: "input",
+        name: "orgPrefix",
         message:
-            'The abbreviated organization name to use as a prefix. Alpha only',
-        default: 'acme',
+          "The abbreviated organization name to use as a prefix. Alpha only",
+        default: "acme",
       },
       {
-        type: 'input',
-        name: 'description',
-        message: 'Plugin Description',
-        default: 'My awesome WordPress plugin',
+        type: "input",
+        name: "description",
+        message: "Plugin Description",
+        default: "My awesome WordPress plugin",
       },
       {
-        type: 'input',
-        name: 'repoLocation',
-        message: 'GIT Repository',
+        type: "input",
+        name: "repoLocation",
+        message: "GIT Repository",
       },
       {
-        type: 'input',
-        name: 'projectHome',
-        message: 'Project homepage',
-        default: 'https://wordpress.org',
+        type: "input",
+        name: "projectHome",
+        message: "Project homepage",
+        default: "https://wordpress.org",
       },
       {
-        type: 'input',
-        name: 'authorName',
-        message: 'Author name',
-        default: 'Awesome Plugin Dev',
+        type: "input",
+        name: "authorName",
+        message: "Author name",
+        default: "Awesome Plugin Dev",
       },
       {
-        type: 'input',
-        name: 'authorEmail',
-        message: 'Author email',
-        default: 'nota@realemail.no',
+        type: "input",
+        name: "authorEmail",
+        message: "Author email",
+        default: "nota@realemail.no",
       },
       {
-        type: 'input',
-        name: 'authorWebsite',
-        message: 'Author homepage',
-        default: 'https://wordpress.org',
+        type: "input",
+        name: "authorWebsite",
+        message: "Author homepage",
+        default: "https://wordpress.org",
       },
     ]);
   }
@@ -134,57 +132,57 @@ module.exports = class extends Generator {
 
     // copy raw files to destination
     [
-      '_.vscode/extensions.json',
-      '_.vscode/settings.json',
-      '_.github/workflows/testing.yml',
-      'src/scripts/starter-script.js',
-      'src/lib/.githold',
-      '_.eslintrc.js',
-      '_.prettierignore',
-      '_.prettierrc.json',
-      'phpcs.xml',
-      'phpunit.xml.dist',
-      '_.zipignore',
+      "_.vscode/extensions.json",
+      "_.vscode/settings.json",
+      "_.github/workflows/testing.yml",
+      "src/scripts/starter-script.js",
+      "src/lib/.githold",
+      "_.eslintrc.js",
+      "_.prettierignore",
+      "_.prettierrc.json",
+      "phpunit.xml.dist",
+      "_.zipignore",
     ].forEach((template) => {
       let destination = template;
-      if (destination[0] == '_') {
+      if (destination[0] == "_") {
         destination = destination.substring(1);
       }
 
       this.fs.copy(
-          this.templatePath(template),
-          this.destinationPath(destination),
+        this.templatePath(template),
+        this.destinationPath(destination)
       );
     });
 
     // process files requiring template vars
     [
-      '_.vscode/launch.json',
-      'uninstall.php',
-      'tests/phpunit/bootstrap.php',
-      'tests/phpunit/test-plugin-file.php',
-      'package.json',
-      '_.gitignore',
-      'composer.json',
-      '_.lando.yml',
-      'Makefile',
-      'README.md',
+      "_.vscode/launch.json",
+      "uninstall.php",
+      "tests/phpunit/bootstrap.php",
+      "tests/phpunit/test-plugin-file.php",
+      "package.json",
+      "_.gitignore",
+      "composer.json",
+      "_.lando.yml",
+      "Makefile",
+      "README.md",
+      "phpcs.xml",
     ].forEach((template) => {
       let destination = template;
-      if (destination[0] == '_') {
+      if (destination[0] == "_") {
         destination = destination.substring(1);
       }
 
       this.fs.copyTpl(
-          this.templatePath(template),
-          this.destinationPath(destination),
-          templateAtts,
+        this.templatePath(template),
+        this.destinationPath(destination),
+        templateAtts
       );
 
       this.fs.copyTpl(
-          this.templatePath('index.php'),
-          this.destinationPath(templateAtts.pluginDash + '.php'),
-          templateAtts,
+        this.templatePath("index.php"),
+        this.destinationPath(templateAtts.pluginDash + ".php"),
+        templateAtts
       );
     });
   }
